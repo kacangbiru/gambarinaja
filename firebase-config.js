2abe23612742f87bcee23c9e3802bb514b813921
kacangbiru <kerupukkecap27@gmail.com>
2025-08-15 10:39:50 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,15 @@
 // firebase-config.js
 // --- GANTI nilai-nilai di bawah dengan config dari Firebase Console (Project settings → SDK setup) ---
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
-};
+    apiKey: "AIzaSyCpfkjN0q7fN5pW9Qex1ZsXMYTbBHpGD2E",
+    authDomain: "gmbr-9918f.firebaseapp.com",
+    databaseURL: "https://gmbr-9918f-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "gmbr-9918f",
+    storageBucket: "gmbr-9918f.firebasestorage.app",
+    messagingSenderId: "183067001431",
+    appId: "1:183067001431:web:451e7b9a47822f9a8e9912",
+    measurementId: "G-DEZ4EEMFK0"
+  };
 
 // Don't modify below (used by app.js)
 window.__FIREBASE_CONFIG__ = firebaseConfig;
